4c6df3809c9cfa00075fccb69ae406a92150cd5c
mkarampatsis <marka@central.ntua.gr>
2025-10-29 20:03:40 +0200

    Update and create for roles

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,7 @@ import roleRoutes from './routes/role.routes';
 const app = express();
 
 app.use(morgan('dev'));
+app.use(express.json());
 
 app.use('/api/users', userRoutes);
 app.use('/api/roles', roleRoutes);
--- src/controllers/role.controller.ts
+++ src/controllers/role.controller.ts
@@ -9,3 +9,23 @@ export const list = async (req:Request, res:Response, next: NextFunction) => {
     next(err);
   }
 }
+
+export const create = async(req:Request, res:Response, next:NextFunction) => {
+  try {
+    console.log(">>", req.body);
+    const result = await roleService.createRole(req.body);
+    res.status(201).json(result);
+  } catch(err) {
+    res.status(401).json({"Error": err});
+    // next(err)
+  }
+}
+
+export const update = async(req: Request,res: Response, next: NextFunction) => {
+  try {
+    const result = await roleService.updateRole(req.params.id!, req.body);
+    res.status(201).json(result);
+  } catch(err) {
+    res.status(401).json({"Error": err});
+  }
+}
