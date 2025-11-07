f73633b8885cb71980204b4b7386c701a010f573
mkarampatsis <marka@central.ntua.gr>
2025-11-07 20:02:03 +0200

    Cors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import cors from 'cors';
 
 import userRoutes  from './routes/user.routes';
 import roleRoutes from './routes/role.routes';
@@ -13,6 +14,11 @@ setupSwagger(app);
 
 app.use(morgan('dev'));
 app.use(express.json());
+
+app.use(cors({
+  // origin: '*'
+  origin: ['http://localhost:4200']
+}));
 
 app.use('/api/users', userRoutes);
 app.use('/api/roles', roleRoutes);
